df74c5854f182e2222931736f2d0867708f6ae66
RyanKaleliGabriel <ryang9241@gmail.com>
2024-01-05 16:42:41 +0300

    Npm Sluglify

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require("slugify");
 const replaceTemplate = require("./modules/replaceTemplate");
 
 ////////////////////////// FILES ////////////
@@ -43,6 +44,9 @@ const tempCard = fs.readFileSync(
   "utf-8"
 );
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs)
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
